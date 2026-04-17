d11548865dbf8ff80cfb327a2388d5af7afe3b40
Gian-windr <gcamposa2@upao.edu.pe>
2026-04-17 01:58:26 +0000

    fix; correción de la imagen

--- iac/api.tf
+++ iac/api.tf
@@ -1,8 +1,8 @@
 resource "docker_container" "api" {
   name  = "api-${terraform.workspace}-01"
-  image = "web02:latest"
+  image = "lab/api"
   ports {
     internal = 3000
     external = var.api_port[terraform.workspace]
   }
-}
+}|
