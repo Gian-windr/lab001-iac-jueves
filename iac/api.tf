9468d2af58f9986968da2e28d41918f2c44e1fef
Gian-windr <gcamposa2@upao.edu.pe>
2026-04-17 01:45:49 +0000

    fix: Se añadió el terraform.workspace para una mayor seguridad en los puertos

--- iac/api.tf
+++ iac/api.tf
@@ -1,9 +1,8 @@
 resource "docker_container" "api" {
   name  = "api-${terraform.workspace}-01"
-  image = "lab/api"
-
-   ports {
-    internal = "3000"
+  image = "web02:latest"
+  ports {
+    internal = 3000
     external = var.api_port[terraform.workspace]
   }
 }
