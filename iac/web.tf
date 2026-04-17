3f97761895f3304922752fede82fd496190aa74f
Gian-windr <gcamposa2@upao.edu.pe>
2026-04-17 01:41:30 +0000

    fix: Se añadió el terraform.workspace para una mayor seguridad en los puertos

--- iac/web.tf
+++ iac/web.tf
@@ -1,9 +1,8 @@
 resource "docker_container" "web" {
   name  = "web-${terraform.workspace}-01"
-  image = "lab/web-"
-
+  image = "web01:latest"
   ports {
-    internal = "80"
-    external = "4001"
+    internal = 80
+    external = var.web_port[terraform.workspace]
   }
 }
